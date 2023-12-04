0744f9797328c2d09dc9106c1ea3b0df2955c67c
R-Sidhartha <rsidhartha20@iitk.ac.in>
2023-12-05 02:57:16 +0530

    changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,12 +6,12 @@ const { body, validationResult } = require("express-validator");
 const userValidationRules = [
   body("first_name").isString().isLength({ min: 3 })
   .withMessage("First Name should be at least 3 characters"),
-  body("last_name").isString().isLength({ min: 3 })
+  body("last_name").isString().isLength({ min: 1 })
   .withMessage("Last Name should be at least 3 characters"),
   body("email").isEmail(),
-  body("gender").isLength({ min: 3 })
-  .withMessage("Gender should be at least 3 characters"),
-  body("avatar").isURL(),
+  body("gender").isString().notEmpty()
+  .withMessage("Gender should not be empty"),
+  body("avatar").isURL().withMessage("Avatar should be a valid URL"),
   body("domain").isString().notEmpty(),
   body("available").isBoolean(),
 ];
